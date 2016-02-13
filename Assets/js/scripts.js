f593cd6cae65f1d0dcfeddb3cbdd1bbfc109d4d9
Tori Hedden <tori.hedden@gmail.com>
2016-02-12 21:08:24 -0500

    Cleans up code that prints inputs to console

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,4 +1,5 @@
 //this targets each component that requires input in the form
+//can you put these all in an array using javascript?
 var firstName = document.getElementById("first-name");
 var lastName = document.getElementById("last-name");
 var emailAddress = document.getElementById("email-address");
@@ -6,37 +7,28 @@ var countryName = document.getElementById("country");
 var biographyContent = document.getElementById("biography");
 var phoneNumber = document.getElementById("phone-number");
 var affiliationsName = document.getElementById("affiliations");
-var occupation = document.getElementById("occupation");
+var userOccupation = document.getElementById("occupation");
+var catName = document.getElementById("cat-name");
+var favGadget = document.getElementById("fav-gadget");
+var talentType = document.getElementById("talent");
+var favDrink = document.getElementById("drink");
+var specialPowers = document.getElementById("powers");
+var weaponChoice = document.getElementById("weapon");
+var userComment = document.getElementById("comments");
+
+//an array of all variables
+var userInputs =[firstName, lastName, emailAddress, countryName, biographyContent, phoneNumber, affiliationsName, userOccupation, catName, favGadget, talentType, favDrink, specialPowers, weaponChoice, userComment];
 
 //this targets the whole form
-var formBody = document.getElementById("whole-form");
+// var formBody = document.getElementById("whole-form");
 
-
-//this targets the submit your data button at the bottom of the screen
+//this targets the "submit your data" button at the bottom of the screen
 var submitButton = document.getElementById("submit");
-
 
 //this prints out the values submitted to the form
 //console.log(the input for each element)
 submitButton.addEventListener("click", function(){
-  console.log(firstName.value);
-  console.log(lastName.value);
-  console.log(emailAddress.value);
-  console.log(countryName.value);
-  console.log(biographyContent.value);
-  console.log(phoneNumber.value);
-  console.log(affiliationsName.value);
-  console.log(occupation.value);
+  for (i=0; i<userInputs.length; i++){
+    console.log(userInputs[i].value);
+  }
 });
-
-
-
-// function myFunction() {
-//     var x = document.getElementById("frm1");
-//     var text = "";
-//     var i;
-//     for (i = 0; i < x.length ;i++) {
-//         text += x.elements[i].value + "<br>";
-//     }
-//     document.getElementById("demo").innerHTML = text;
-// }
