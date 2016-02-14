3062c5cd75d7df6e13f7d513cfb632e4e50272bf
Tori Hedden <tori.hedden@gmail.com>
2016-02-14 17:50:08 -0500

    Adds event listener to every text box and text field to change the border color on click

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -24,7 +24,7 @@ var submitButton = document.getElementById("submit");
 
 //this prints out the values submitted to the form
 //console.log(the input for each element)
-//then, the form is cleared. all values input are cleared
+//then, the form is cleared. all values input are cleared AKA set to an empty string.
 submitButton.addEventListener("click", function(){
   for (i=0; i<userInputs.length; i++){
     console.log(userInputs[i].value);
@@ -33,3 +33,24 @@ submitButton.addEventListener("click", function(){
     userInputs[i].value="";
   }
 });
+
+//this targets the text field and text area elements, which have been given a class of "white-border" by default
+var colorChange = document.getElementsByClassName("white-border");
+
+for (i = 0; i < colorChange.length; i++){
+  ( function(){
+    var currentChangedElement = colorChange[i];
+    currentChangedElement.addEventListener ("click", function (){
+      var blueBorder = document.getElementsByClassName("blue-border");
+        for (j = 0; j < blueBorder.length; j++) {
+          blueBorder[j].className = "white-border";
+        }
+      currentChangedElement.className = "blue-border";
+    })
+  }())
+};
+
+//dealing with the problem of assigning event listeners to every element in an array
+//the event listener was only applying to the last element in the array (the for loop looped through every element in the array, then applied the className change to the last element, NOT the element that was clicked)
+//was able to base solution off of the discussion found here:
+//http://stackoverflow.com/questions/19586137/addeventlistener-using-for-loop-and-passing-values
